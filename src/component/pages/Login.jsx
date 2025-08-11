2405e1102a6d12e32cca519c75f1471a61fd1571
indepthspace <vividmemories_@naver.com>
2025-08-11 13:52:36 +0900

    🔥 로그인수정

--- src/component/pages/Login.jsx
+++ src/component/pages/Login.jsx
@@ -1,7 +1,28 @@
 const Login =()=> {
   return(
     <>
-      2
+      <div className='wrap'>
+         <h1><img src="/lifemindlogo.png" alt="lifemind" /></h1>
+
+         <form>
+          <fieldset>
+            <legend>회원 로그인</legend>
+
+            <div className="input-box">
+              <input type="text" placeholder='아이디입력' />
+              <input type="password" placeholder='비밀번호입력'/>
+              <button type='submit'>로그인</button>
+            </div>{/* .input-box */}
+
+            <div className="find-join">
+              <a href="#">비밀번호 찾기</a>
+              <a href="#">회원가입</a>
+            </div>{/* .find-join */}
+
+          </fieldset>
+        </form>
+
+      </div>
     </>
   )
 }
